0e359b4aea1f5e555b2573202edb7c7d48434809
Arita Osmani <aritaaosmani@gmail.com>
2022-02-23 00:20:36 +0100

    Added delete and update

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import Database from 'better-sqlite3'
+import e from 'cors'
 
 const db = new Database('./interviews.db', {
     verbose: console.log
@@ -32,6 +33,8 @@ const getInterviewersByApplicantId = db.prepare(`
   
 `)
 
+
+
 const getInterviewers = db.prepare(`
     SELECT * FROM interviewers;
 `)
@@ -39,10 +42,6 @@ const getInterviewers = db.prepare(`
 const getInterviewersById = db.prepare(`
     SELECT * FROM interviewers WHERE id = ?;
 `)
-const createInterviewer = db.prepare(`
-    INSERT INTO interviewers (name,email) VALUES (?,?);
-`)
-
 const getApplicantsByInterviewer = db.prepare(`
     SELECT applicants.*,interviews.date,interviews.score
     FROM applicants 
@@ -50,6 +49,10 @@ const getApplicantsByInterviewer = db.prepare(`
     ON applicants.id = interviews.applicantId
     WHERE interviews.interviewerId = ?;
 `)
+const createInterviewer = db.prepare(`
+    INSERT INTO interviewers (name,email) VALUES (?,?);
+`)
+
 
 const createApplicant = db.prepare(`
     INSERT INTO applicants (name,email) VALUES (?,?);
@@ -63,6 +66,35 @@ const getInterviewsById = db.prepare(`
     SELECT * FROM interviews WHERE id = ?;
 `)
 
+const deleteInterview = db.prepare(`
+    DELETE FROM interviews WHERE id =?;
+`)
+
+const deleteApplicant = db.prepare(`
+    DELETE FROM applicants WHERE id = ?;
+`)
+const deleteInterviewer = db.prepare(`
+    DELETE FROM interviewers WHERE id =?;
+`)
+
+const deleteInterviewByApplicant = db.prepare(`
+    DELETE FROM interviews WHERE applicantId = ?;
+`)
+
+const deleteInterviewByInterviewer = db.prepare(`
+    DELETE FROM interviews WHERE interviewerId = ?;
+`)
+
+const updateApplicant = db.prepare(`
+    UPDATE applicants SET name =?, email = ? WHERE id =?;
+`)
+const updateInterviewer = db.prepare(`
+    UPDATE interviewers SET name =?, email = ? WHERE id =?;
+`)
+const updateInterview = db.prepare(`
+    UPDATE interviews SET applicantId = ?, interviewerId = ?, date = ?, score = ? WHERE id=?;
+`)
+
 app.get('/applicants', (req, res) => {
     const applicants = getApplicants.all()
 
@@ -73,6 +105,18 @@ app.get('/applicants', (req, res) => {
     res.send(applicants)
 })
 
+app.get('/applicants/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getApplicantById.get(id)
+    if (match) {
+        const interviews = getInterviewersByApplicantId.all(id)
+        match.interviews = interviews;
+        res.send(match);
+    } else {
+        res.status(404).send({ error: 'Applicant not found!' })
+    }
+})
+
 app.get('/interviewers', (req, res) => {
     const interviewers = getInterviewers.all();
 
@@ -81,6 +125,19 @@ app.get('/interviewers', (req, res) => {
         interviewer.interviews = interviews;
     }
     res.send(interviewers)
+})
+
+app.get('/interviewers/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getInterviewersById.get(id);
+
+    if (match) {
+        const interviews = getApplicantsByInterviewer.all(id)
+        match.interviews = interviews;
+        res.send(match)
+    } else {
+        res.status(404).send({ error: 'Interviewer not found!' })
+    }
 })
 
 app.post('/applicants', (req, res) => {
@@ -158,6 +215,155 @@ app.post('/interviews', (req, res) => {
         res.status(400).send(errors)
     }
 
+})
+
+app.delete('/interviews/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getInterviewsById.get(id);
+
+    if (match) {
+        const result = deleteInterview.run(id)
+        if (result.changes !== 0) {
+            res.send({ message: 'Interview deleted sucessfully!' })
+        } else {
+            res.status(400).send({ error: 'Something went wrong!' })
+        }
+    } else {
+        res.status(404).send({ error: 'Interview not found!' })
+    }
+})
+
+app.delete('/applicants/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getApplicantById.get(id)
+
+    if (match) {
+        deleteInterviewByApplicant.run(id);
+        const result = deleteApplicant.run(id)
+        if (result.changes !== 0) {
+            res.send({ message: 'Applicant deleted sucessfully!' })
+        } else {
+            res.status(400).send({ error: 'Something went wrong!' })
+        }
+
+    } else {
+        res.status(404).send({ error: 'Applicant not found!' })
+    }
+
+})
+
+app.delete('/interviewers/:id', (req, res) => {
+    const id = req.params.id;
+    const match = getInterviewersById.get(id);
+
+    if (match) {
+        deleteInterviewByInterviewer.run(id)
+        const result = deleteInterviewer.run(id)
+        if (result.changes !== 0) {
+            res.send({ message: 'Interviewer deleted sucessfully!' })
+        } else {
+            res.status(400).send({ error: 'Something went wrong!' })
+        }
+    } else {
+        res.status(404).send({ error: 'Interviewer not found!' })
+    }
+})
+
+app.patch('/applicants/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, email } = req.body;
+    const match = getApplicantById.get(id);
+    const errors = [];
+
+    if (name && typeof name !== 'string') {
+        errors.push({ error: 'Name not a string' })
+    }
+    if (email && typeof email !== 'string') {
+        errors.push({ error: 'Email not a string!' })
+    }
+
+    if (errors.length === 0) {
+        if (match) {
+            const result = updateApplicant.run(name ?? match.name, email ?? match.email, id)
+            const updated = getApplicantById.get(id)
+            res.send(updated)
+        } else {
+            res.status(404).send({ error: 'Applicant not found!' })
+        }
+    } else {
+        res.status(400).send(errors)
+    }
+
+})
+
+app.patch('/interviewers/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, email } = req.body;
+    const match = getInterviewersById.get(id);
+    const errors = [];
+
+    if (name && typeof name !== 'string') {
+        errors.push({ error: 'Name not a string' })
+    }
+    if (email && typeof email !== 'string') {
+        errors.push({ error: 'Email not a string!' })
+    }
+
+    if (errors.length === 0) {
+        if (match) {
+            const result = updateInterviewer.run(name ?? match.name, email ?? match.email, id)
+            const updated = getInterviewersById.get(id)
+            res.send(updated)
+        } else {
+            res.status(404).send({ error: 'Interviewer not found!' })
+        }
+    } else {
+        res.status(400).send(errors)
+    }
+
+})
+
+app.patch('/interviews/:id', (req, res) => {
+    const id = req.params.id;
+    const { applicantId, interviewerId, date, score } = req.body;
+    const match = getInterviewsById.get(id);
+
+    const errors = []
+
+    if (applicantId && typeof applicantId !== 'number') {
+        errors.push({ error: 'Applicant id not a number!' })
+    }
+    if (interviewerId && typeof interviewerId !== 'number') {
+        errors.push({ error: 'Interview id not a number!' })
+    }
+    if (date && typeof date !== 'string') {
+        errors.push({ error: 'Date not a string!' })
+    }
+    if (score && typeof score !== 'number') {
+        errors.push({ error: 'Score not a number!' })
+    }
+    if (score && score < 1 || score > 10) {
+        errors.push({ error: 'Score is outside range.It should be inside the interval [1,10]' })
+    }
+
+    if (errors.length === 0) {
+        if (match) {
+            const applicant = getApplicantById.get(applicantId)
+            const interviewer = getInterviewersById.get(interviewerId)
+
+            if (applicant && interviewer) {
+                const result = updateInterview.run(applicantId ?? match.applicantId, interviewerId ?? match.interviewerId, date ?? match.date, score ?? match.score, id);
+                const updated = getInterviewsById.get(id)
+                res.send(updated)
+            } else {
+                res.status(404).send({ error: 'Applicant or Interviewer not found!' })
+            }
+        } else {
+            res.status(404).send({ error: 'Interview not found!' })
+        }
+    } else {
+        res.status(400).send(errors)
+    }
 })
 
 app.listen(PORT, () => {
